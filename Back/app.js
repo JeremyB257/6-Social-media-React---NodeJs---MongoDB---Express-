0da07cbd7b54e4228f449cbb092fb25297915fff
JeremyB257 <bertelootjerem@gmail.com>
2022-09-08 17:15:13 +0200

    reorganize folder

---
+++ Back/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/auth.middleware');
+
+app.use(cookieParser());
+
+const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
+
+mongoose
+  .connect(
+    'mongodb+srv://' +
+      process.env.DB_CONNECT +
+      '@cluster0.devgshf.mongodb.net/' +
+      process.env.DB_NAME +
+      '?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
+
+module.exports = app;
